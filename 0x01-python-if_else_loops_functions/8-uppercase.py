285470c40b93180549399e6c0d109520ee14887b
Omosquary <iamomosquare@gmail.com>
2022-06-28 03:37:44 -0700

    Mandatory Task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+def uppercase(str):
+    for upper in str:
+        if ord(upper) > 96 and ord(upper) < 123:
+            upper = chr(ord(upper) - 32)
+        print("{}".format(upper), end="")
+    print("")
+
+
