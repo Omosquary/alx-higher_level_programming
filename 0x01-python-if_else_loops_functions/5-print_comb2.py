09edf60b99f5a4bd4f78242488bf61737e2727d8
Omosquary <iamomosquare@gmail.com>
2022-06-28 04:10:53 -0700

    Update Task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for numbers in range(0, 100):
     if numbers == 99:
-        print("{0:02}".format(numbers))
+        print("{}".format(numbers))
     else:
-        print("{0:02}".format(numbers), end=",")
+        print("{:02}".format(numbers), end=",")
