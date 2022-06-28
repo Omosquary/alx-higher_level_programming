744d08ec6fe3b471ebcc03e78b696f6f1d5a5fc3
Omosquary <iamomosquare@gmail.com>
2022-06-28 04:16:01 -0700

    Update Task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,4 +4,4 @@ for num1 in range(0, 10):
         if num1 == 8 and num2 == 9:
             print("{0}{1}".format(num1, num2))
         else:
-            print("{0}{1}".format(num1, num2), end=",")
+            print("{0}{1}".format(num1, num2), end=", ")
