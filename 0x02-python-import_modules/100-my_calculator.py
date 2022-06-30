d66123b84f45b47948abe4e2888282d9a8f9edc1
Omosquary <iamomosquare@gmail.com>
2022-06-30 04:19:17 -0700

    Update task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     import sys
 
     if len(sys.argv) - 1 != 3:
-        print(" Usage: ./100-my_calculator.py <a> <operator> <b>")
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
     sign = {"+": add, "-": sub, "*": mul, "/": div}
