d1d3943ef2c865c19586c40e79ec28787334595f
Omosquary <iamomosquare@gmail.com>
2022-10-09 08:30:38 -0700

    Task 0 Correction - Get all States

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,7 +6,7 @@ import sys
 
 if __name__ == "__main__":
     database = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+                               passwd=sys.argv[2], db=sys.argv[3], port=3306)
     curs = database.cursor()
     curs.execute("SELECT * FROM states")
     rows = curs.fetchall()
